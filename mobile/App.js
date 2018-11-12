a73e3f88cb7f1c89852eddde09a6d3089c187ea0
rafaelcastro <rafc@seas.upenn.edu>
2018-11-12 15:17:50 -0500

    Implemented splash screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,71 +1,89 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
+import {Platform, StatusBar, StyleSheet, View, Image} from 'react-native';
+import {AppLoading, Asset, Font, Icon} from 'expo';
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
 
 import AppNavigator from './navigation/AppNavigator';
 import reducer from './reducers/'
-import Splash from 'screens/Splash';
 
 const store = createStore(reducer);
 
 export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
+    state = {
+        isSplashReady: false,
+        isAppReady: false,
+    };
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <Provider store={store}>
-          <View style={styles.container}>
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-            <AppNavigator />
-          </View>
-        </Provider>
-      );
+    render() {
+        if (!this.state.isSplashReady) {
+            return (
+                <AppLoading
+                    //Load Splash Screen x
+                    startAsync={this._cacheSplashResourcesAsync}
+                    onFinish={() => this.setState({isSplashReady: true})}
+                    onError={console.warn}
+                    autoHideSplash={false}
+                />
+            );
+        }
+        if (!this.state.isAppReady) {
+            return (
+                <View style={{flex: 1}}>
+                    <Image
+                        source={require('./assets/images/splash.png')}
+                        onLoad={this._cacheResourcesAsync}
+                    />
+                </View>
+            );
+        }
+
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    {Platform.OS === 'ios' && <StatusBar barStyle="default"/>}
+                    <AppNavigator/>
+                </View>
+            </Provider>
+        );
     }
-  }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+    // Load the graphics for the splash screen
+    _cacheSplashResourcesAsync = async () => {
+        const png = require('./assets/images/splash.png');
+        return Asset.fromModule(png).downloadAsync()
+    };
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    // Load the resources that we need for our app
+    _cacheResourcesAsync = async () => {
+        Expo.SplashScreen.hide();
+        await Promise.all([
+            Asset.loadAsync([
+                require('./assets/images/robot-dev.png'),
+                require('./assets/images/robot-prod.png'),
+            ]),
+            Font.loadAsync({
+                // This is the font that we are using for our tab bar
+                ...Icon.Ionicons.font,
+                // Fonts we are using in our app
+                // Todo: Remove Space mongo from all screens and the delete it here.
+                'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+                'montserrat': require('./assets/fonts/Montserrat-Regular.ttf'),
+
+            }),
+        ]);
+        this.setState({isAppReady: true});
+
+    };
+
+
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
