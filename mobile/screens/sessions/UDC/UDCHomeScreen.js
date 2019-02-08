13de3bd2c8a3f64b9b028b8b68e698331b596743
Desmond Howard <desmondh@seas.upenn.edu>
2019-02-07 19:58:17 -0500

    poll home screen every 5 seconds

--- mobile/screens/sessions/UDC/UDCHomeScreen.js
+++ mobile/screens/sessions/UDC/UDCHomeScreen.js
@@ -32,7 +32,7 @@ import { request } from "../../../components/helpers";
         //Get all UDCs whenever user opens this screen
         this.props.dispatch(getAllUDC());
         //Keep fetching for data every minute.
-        this.interval = setInterval(() => this.getAllUDCs, 10* 1000);
+        this.interval = setInterval(() => this.getAllUDCs, 5000);
     }
 
     getAllUDCs(){
